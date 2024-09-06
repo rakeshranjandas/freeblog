2136dea8957d987703ead658f55ead1a136a937b
Rakesh <rakesh.das@mindfiresolutions.com>
2024-09-07 00:19:28 +0530

    Add Tags

--- index.php
+++ index.php
@@ -18,7 +18,24 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js" integrity="sha512-YJgZG+6o3xSc0k5wv774GS+W1gx0vuSI/kr0E0UylL/Qg/noNspPtYwHPN9q6n59CTR/uhgXfjDXLTRI+uIryg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/ui/trumbowyg.min.css" integrity="sha512-Fm8kRNVGCBZn0sPmwJbVXlqfJmPC13zRsMElZenX6v721g/H7OukJd8XzDEBRQ2FSATK8xNF9UYvzsCtUpfeJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+ <style>
+        /*.tags-input {
+            display: flex;
+            flex-wrap: wrap;
+            border: 1px solid #dbdbdb;
+            border-radius: 4px;
+            padding: 0.5em;
+        }*/
+       /* .tags-input input {
+            border: none;
+            outline: none;
+            flex-grow: 1;
+        }
+        .tags-input .tag {
+            margin: 0.25em;
+        }*/
 
+    </style>
 </head>
 <body>
 	<section class="section">
@@ -26,15 +43,23 @@
 
 			<div class="container column is-three-quarters">
 				
-				<input class="block input is-link" type="text" placeholder="Title" />
+				<div class="field">
+					<input class="input is-link" type="text" placeholder="Title" />
+				</div>
 				
-				<input class="block input is-link" type="text" placeholder="Tags" />
+				<div class="field">
+					<div class="tags-input is-grouped is-grouped-multiline" id="tagsInputContainer">
+						 <input class="input is-link" id="tagsInput" type="text" placeholder="Add a tag">
+					</div>
+				</div>
 				
-				<div id="editor_area" class="block">
+				<div id="editor_area" class="field">
 					<div id="editor_editor"></div>
 				</div>
 
-				<p class="content has-text-centered block"><button class="button is-primary">Publish</button></p>
+				<div class="field">
+					<p class="content has-text-centered"><button class="button is-primary">Publish</button></p>
+				</div>
 			</div>
 
 			<div id="folder_area" class="container column is-one-quarter has-background-light">
@@ -44,6 +69,10 @@
 
 		</div>
 	</section>
+
+    <script>
+
+    </script>
 
 
     <!-- Link to External JavaScript -->
--- js/script.js
+++ js/script.js
@@ -3,6 +3,54 @@
 
 $(document).ready(function() {
 
-	$('#editor_editor').trumbowyg();
+	initEditor();
+
+	initTagsInput();
 
 })
+
+
+function initEditor() {
+	
+	$('#editor_editor').trumbowyg();
+
+}
+
+function initTagsInput() {
+	
+	const tagsInput = $('#tagsInput');
+    
+    const tags = [];
+    
+    // Function to add a new tag
+    function addTag(tag) {
+        if (tags.includes(tag)) return;
+        
+        tags.push(tag);
+        
+        const tagElement = $('<span/>');
+        tagElement.addClass('tag is-info m-2');
+        tagElement.text(tag);
+        
+        const deleteBtn = $('<button/>');
+        deleteBtn.addClass('delete is-small');
+        deleteBtn.on('click', function() {
+            $(this).closest('span').remove();
+            tags.splice(tags.indexOf(tag), 1);
+        });
+        
+        tagElement.append(deleteBtn);
+        tagsInput.after(tagElement);
+
+        tagsInput.val('');
+    }
+
+    // Handle pressing Enter to add a tag
+    tagsInput.on('keypress', function(e) {
+        if (e.key === 'Enter' && tagsInput.val().trim()) {
+            e.preventDefault();
+            addTag(tagsInput.val().trim());
+        }
+    });
+
+}
